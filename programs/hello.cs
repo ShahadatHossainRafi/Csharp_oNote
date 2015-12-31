9f4dc4e88a3e0e65bf5327da22cb18f5c44f4471
engr-rafi <rafi.lmu@gmail.com>
2016-01-01 01:08:26 +0600

    prgrams folder added

---
+++ programs/hello.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ProBook
+ * Date: 1/1/2016
+ * Time: 12:51 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+
+namespace BismillahApp{
+
+	class Bismillah{
+		static void Main(string[] args){
+			Console.WriteLine("Bismillahir Rahmanir Rahim");
+		}
+	}
+}
