b75bcba304067399e53af71e973013eaf1bd02f3
Shahadat Hossain <rafi@in2rafi.com>
2016-01-01 01:10:06 +0600

    hello.cs program edited

--- programs/hello.cs
+++ programs/hello.cs
@@ -1,10 +1,9 @@
 ﻿/*
- * Created by SharpDevelop.
- * User: ProBook
+ * Created by Shahadat Hossain.
+ * User: Shahadat Hossain
  * Date: 1/1/2016
  * Time: 12:51 AM
  * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 
 using System;
